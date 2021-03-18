012ee28aedf164af65c9e679673df570af08aa24
Thomas Deblock <deblock.thomas.62@gmail.com>
2021-03-18 08:14:04 +0100

    feat: update the number matcher now 10.0 match 10

--- README.md
+++ README.md
@@ -18,12 +18,12 @@ maven
 
 example:
 ```java
-final var expectedJson = "{\"foo\": \"bar\"}";
-final var receivedJson = "{\"foo\": \"foo\"}";
+final var expectedJson = "{\"foo\": \"bar\", \"bar\": \"bar\", \"numberMatch\": 10.0, \"numberUnmatched\": 10.01}";
+final var receivedJson = "{\"foo\": \"foo\", \"bar\": \"bar\", \"numberMatch\": 10, \"numberUnmatched\": 10.02}";
 
 // define your matcher
 // CompositeJsonMatcher use other matcher to perform matching on objects, list or primitive
-final var jsonMatcher = new CompositeJsonMatcher( 
+final var jsonMatcher = new CompositeJsonMatcher(
     new LenientJsonArrayPartialMatcher(), // comparing array using lenient mode (ignore array order and extra items)
     new LenientJsonObjectPartialMatcher(), // comparing object using lenient mode (ignoring extra properties)
     new PrimitivePartialMatcher()
@@ -43,7 +43,9 @@ System.out.println(jsondiff.similarityRate());
 ```
 Result:
 ```
+The item "$.numberUnmatched" didn't match. Expected "10.01", Received: "10.02"
 The item "$.foo" didn't match. Expected "bar", Received: "foo"
-50.0
+
+75.0
 ```
 
--- src/test/java/com/deblock/Test.java
+++ src/test/java/com/deblock/Test.java
@@ -6,27 +6,27 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 
 public class Test {
     public static void main(String[] args) throws JsonProcessingException {
-        final var expectedJson = "{\"foo\": \"bar\"}";
-        final var receivedJson = "{\"foo\": \"foo\"}";
+        final var expectedJson = "{\"foo\": \"bar\", \"bar\": \"bar\", \"numberMatch\": 10.0, \"numberUnmatched\": 10.01}";
+        final var receivedJson = "{\"foo\": \"foo\", \"bar\": \"bar\", \"numberMatch\": 10, \"numberUnmatched\": 10.02}";
 
-// define your matcher
-// CompositeJsonMatcher use other matcher to perform matching on objects, list or primitive
+        // define your matcher
+        // CompositeJsonMatcher use other matcher to perform matching on objects, list or primitive
         final var jsonMatcher = new CompositeJsonMatcher(
                 new LenientJsonArrayPartialMatcher(), // comparing array using lenient mode (ignore array order and extra items)
                 new LenientJsonObjectPartialMatcher(), // comparing object using lenient mode (ignoring extra properties)
                 new PrimitivePartialMatcher()
         );
 
-// generate a diff
+        // generate a diff
         final var jsondiff = DiffGenerator.diff(expectedJson, receivedJson, jsonMatcher);
 
-// use the viewer to collect diff data
+        // use the viewer to collect diff data
         final var viewer = new OnlyErrorDiffViewer();
         jsondiff.display(viewer);
 
-// print the diff result
+        // print the diff result
         System.out.println(viewer);
-// print a similarity ratio between expected and received json (0 <= ratio <= 100)
+        // print a similarity ratio between expected and received json (0 <= ratio <= 100)
         System.out.println(jsondiff.similarityRate());
     }
 }
