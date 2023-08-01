c0650c5a147eedfd9275d8c9ebd11e3be948ef13
Bart van Deenen <bartvandeenen@streammachine.io>
2023-08-01 09:03:01 +0200

    test: test for strict patch

--- src/test/java/com/deblock/jsondiff/viewer/PatchDiffViewerTest.java
+++ src/test/java/com/deblock/jsondiff/viewer/PatchDiffViewerTest.java
@@ -5,6 +5,7 @@ import com.deblock.jsondiff.matcher.CompositeJsonMatcher;
 import com.deblock.jsondiff.matcher.LenientJsonArrayPartialMatcher;
 import com.deblock.jsondiff.matcher.LenientJsonObjectPartialMatcher;
 import com.deblock.jsondiff.matcher.LenientNumberPrimitivePartialMatcher;
+import com.deblock.jsondiff.matcher.StrictJsonObjectPartialMatcher;
 import com.deblock.jsondiff.matcher.StrictPrimitivePartialMatcher;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -19,11 +20,28 @@ public class PatchDiffViewerTest {
     public void diffTestUsingLenientDiff() throws IOException {
         final var actualJson = Files.readString(Path.of("src/test/java/com/deblock/jsondiff/viewer/actual.json"));
         final var expectedJson = Files.readString(Path.of("src/test/java/com/deblock/jsondiff/viewer/expected.json"));
-        final var expectedDiff = Files.readString(Path.of("src/test/java/com/deblock/jsondiff/viewer/lenientDiff.patch"));
+        final var expectedDiff = Files.readString(Path.of("src/test/java/com/deblock/jsondiff/viewer/lenientDiff.patch")).trim();
         final var jsonMatcher = new CompositeJsonMatcher(
                 new LenientJsonArrayPartialMatcher(),
                 new LenientJsonObjectPartialMatcher(),
                 new LenientNumberPrimitivePartialMatcher(new StrictPrimitivePartialMatcher())
+        );
+        final var jsondiff = DiffGenerator.diff(expectedJson, actualJson, jsonMatcher);
+
+        final var patchResult= PatchDiffViewer.from(jsondiff);
+
+        Assertions.assertEquals(expectedDiff, patchResult.toString());
+    }
+
+    @Test
+    public void diffTestUsingStrictDiff() throws IOException {
+        final var actualJson = Files.readString(Path.of("src/test/java/com/deblock/jsondiff/viewer/actual.json"));
+        final var expectedJson = Files.readString(Path.of("src/test/java/com/deblock/jsondiff/viewer/expected.json"));
+        final var expectedDiff = Files.readString(Path.of("src/test/java/com/deblock/jsondiff/viewer/strictDiff.patch")).trim();
+        final var jsonMatcher = new CompositeJsonMatcher(
+            new LenientJsonArrayPartialMatcher(),
+            new StrictJsonObjectPartialMatcher(),
+            new LenientNumberPrimitivePartialMatcher(new StrictPrimitivePartialMatcher())
         );
         final var jsondiff = DiffGenerator.diff(expectedJson, actualJson, jsonMatcher);
 
