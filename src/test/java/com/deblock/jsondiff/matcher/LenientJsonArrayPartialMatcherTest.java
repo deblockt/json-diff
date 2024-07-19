56a38af36423027e254c56a59903064bf5440535
Thomas Deblock <deblock.thomas.62@gmail.com>
2024-07-19 08:40:09 +0200

    feat: optimize json array partial matcher for big json file (#19)

--- src/test/java/com/deblock/jsondiff/matcher/LenientJsonArrayPartialMatcherTest.java
+++ src/test/java/com/deblock/jsondiff/matcher/LenientJsonArrayPartialMatcherTest.java
@@ -117,6 +117,42 @@ class LenientJsonArrayPartialMatcherTest {
                 .validate(result);
     }
 
+    @Test
+    void shouldWorkWithDuplicatedArrayItemsOnExpected() {
+        final var expected = new ArrayNode(null, List.of(TextNode.valueOf("a"), TextNode.valueOf("a"), TextNode.valueOf("b")));
+        final var actual = new ArrayNode(null, List.of(TextNode.valueOf("a"), TextNode.valueOf("b")));
+        final var jsonMatcher = Mockito.mock(JsonMatcher.class);
+        Mockito.when(jsonMatcher.diff(any(), any(), any())).thenAnswer(this::matchByEquality);
+
+        final var result = new LenientJsonArrayPartialMatcher().jsonDiff(path, expected, actual, jsonMatcher);
+
+        assertEquals(path, result.path());
+        new JsonDiffAsserter()
+                .assertSimilarityRate(66.66)
+                .assertMatchingProperty(path.add(Path.PathItem.of(0)))
+                .assertMissingProperty(path.add(Path.PathItem.of(1)))
+                .assertMatchingProperty(path.add(Path.PathItem.of(2)))
+                .validate(result);
+    }
+
+    @Test
+    void shouldWorkWithDuplicatedArrayItemsOnActual() {
+        final var expected = new ArrayNode(null, List.of(TextNode.valueOf("a"), TextNode.valueOf("b")));
+        final var actual = new ArrayNode(null, List.of(TextNode.valueOf("a"), TextNode.valueOf("a"), TextNode.valueOf("b")));
+        final var jsonMatcher = Mockito.mock(JsonMatcher.class);
+        Mockito.when(jsonMatcher.diff(any(), any(), any())).thenAnswer(this::matchByEquality);
+
+        final var result = new LenientJsonArrayPartialMatcher().jsonDiff(path, expected, actual, jsonMatcher);
+
+        assertEquals(path, result.path());
+        new JsonDiffAsserter()
+                .assertSimilarityRate(66.66)
+                .assertMatchingProperty(path.add(Path.PathItem.of(0)))
+                .assertExtraProperty(path.add(Path.PathItem.of(1)))
+                .assertMatchingProperty(path.add(Path.PathItem.of(1)))
+                .validate(result);
+    }
+
     private JsonDiff matchByEquality(org.mockito.invocation.InvocationOnMock args) {
         if (args.getArgument(1).equals(args.getArgument(2))) {
             return fullMatchJsonDiff(args.getArgument(0));
